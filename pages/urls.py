e98ef8eb6b5986a291d886ec17b8b0747028e3dc
Memet <mkoslu@protonmail.com>
2024-08-11 15:24:46 +0300

    added tahini page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 # pages/urls.py
 # pages/urls.py
 from django.urls import path
-from .views import ChatBotTemplateView
+from .views import ChatBotTemplateView, tahini_view
 
 urlpatterns = [
     path('', ChatBotTemplateView.as_view(), name='home'),
+    path('tahini/', tahini_view, name='tahini'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 # pages/views.py
-
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class ChatBotTemplateView(TemplateView):
     template_name = 'pages/chatbot.html'
+ 
+def tahini_view(request):
+    return render(request, 'pages/tahini_page.html') 
