8d9dd1920cf1a9bea5afcfa91b372210c3f7e019
Memet <mkoslu@protonmail.com>
2024-09-07 10:50:36 +0300

    Update admin.py for custom user model

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# user_management/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+User = get_user_model()
+
+class UserAdmin(DefaultUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
