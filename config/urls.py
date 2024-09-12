61c96806d13a45d75089dba6aa785079a4b360fa
Memet <mkoslu@protonmail.com>
2024-09-12 13:02:21 +0300

    dealing with images

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
 # config/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path,include
@@ -8,4 +10,4 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('', include('pages.urls')),
     path('api/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
