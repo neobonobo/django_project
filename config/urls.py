fbb704324e9e24dc46b2efdac33dc04c5fc05afa
Memet <mkoslu@protonmail.com>
2024-09-28 23:31:20 +0300

    styling

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path,include
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('api/', include('api.urls')),
     path('daytrack/', include('daytrack.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
